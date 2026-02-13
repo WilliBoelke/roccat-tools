129a21b946b4cc91b4d05663ac67b737c2d18b1d
Willi Bölke <williboelke-dev@outlook.com>
2026-02-14 00:17:46 +0100

    all buttons are now working correcly

--- tyon/libroccattyon/leadr_button_mapping.h
+++ tyon/libroccattyon/leadr_button_mapping.h
@@ -15,22 +15,22 @@
  *   then set leadr_button_map[X] = Y
  */
 static const guint leadr_button_map[16] = {
-	0,  // TYON_BUTTON_INDEX_LEFT (0) -> correct
-	1,  // TYON_BUTTON_INDEX_RIGHT (1) -> correct
-	2,  // TYON_BUTTON_INDEX_MIDDLE (2) -> correct
-	3,  // TYON_BUTTON_INDEX_THUMB_BACK (3) -> correct
-	11, // TYON_BUTTON_INDEX_THUMB_FORWARD (4) -> swap with wheel_down
-	13, // TYON_BUTTON_INDEX_THUMB_PEDAL (5) -> correct
-	14, // TYON_BUTTON_INDEX_THUMB_PADDLE_UP (6) -> correct
-	4,  // TYON_BUTTON_INDEX_THUMB_PADDLE_DOWN (7) -> swap with wheel_down (4)
-	8,  // TYON_BUTTON_INDEX_LEFT_BACK (8) -> correct
-	5,  // TYON_BUTTON_INDEX_LEFT_FORWARD (9) -> maps to physical 5
-	10, // TYON_BUTTON_INDEX_RIGHT_BACK (10) -> correct
-	7,  // TYON_BUTTON_INDEX_RIGHT_FORWARD (11) -> correct
-	12, // TYON_BUTTON_INDEX_FIN_RIGHT (12) -> correct
-	9,  // TYON_BUTTON_INDEX_FIN_LEFT (13) -> correct
-	6,  // TYON_BUTTON_INDEX_WHEEL_UP (14) -> swap with thumb_paddle_up (6)
-	15, // TYON_BUTTON_INDEX_WHEEL_DOWN (15) -> swap with thumb_forward
+	0,  // TYON_BUTTON_INDEX_LEFT (0)
+	1,  // TYON_BUTTON_INDEX_RIGHT (1)
+	2,  // TYON_BUTTON_INDEX_MIDDLE (2)
+	12, // TYON_BUTTON_INDEX_THUMB_BACK (3) -> switched with LEFT_BACK
+	11, // TYON_BUTTON_INDEX_THUMB_FORWARD (4)
+	13, // TYON_BUTTON_INDEX_THUMB_PEDAL (5)
+	14, // TYON_BUTTON_INDEX_THUMB_PADDLE_UP (6)
+	15, // TYON_BUTTON_INDEX_THUMB_PADDLE_DOWN (7)
+	8,  // TYON_BUTTON_INDEX_LEFT_BACK (8) -> switched with THUMB_BACK
+	5,  // TYON_BUTTON_INDEX_LEFT_FORWARD (9)
+	6,  // TYON_BUTTON_INDEX_RIGHT_BACK (10)
+	7,  // TYON_BUTTON_INDEX_RIGHT_FORWARD (11)
+	10, // TYON_BUTTON_INDEX_FIN_RIGHT (12)
+	9,  // TYON_BUTTON_INDEX_FIN_LEFT (13)
+	3,  // TYON_BUTTON_INDEX_WHEEL_UP (14)
+	4,  // TYON_BUTTON_INDEX_WHEEL_DOWN (15)
 };
 
 /* Inverse mapping: leadr_physical_index -> tyon_index */
@@ -38,19 +38,19 @@ static const guint leadr_button_map_inverse[16] = {
 	0,  // Physical button 0 -> Tyon index 0 (LEFT)
 	1,  // Physical button 1 -> Tyon index 1 (RIGHT)
 	2,  // Physical button 2 -> Tyon index 2 (MIDDLE)
-	3,  // Physical button 3 -> Tyon index 3 (THUMB_BACK)
-	7,  // Physical button 4 -> Tyon index 7 (THUMB_PADDLE_DOWN)
+	14, // Physical button 3 -> Tyon index 14 (WHEEL_UP)
+	15, // Physical button 4 -> Tyon index 15 (WHEEL_DOWN)
 	9,  // Physical button 5 -> Tyon index 9 (LEFT_FORWARD)
-	14, // Physical button 6 -> Tyon index 14 (WHEEL_UP)
+	10, // Physical button 6 -> Tyon index 10 (RIGHT_BACK)
 	11, // Physical button 7 -> Tyon index 11 (RIGHT_FORWARD)
-	8,  // Physical button 8 -> Tyon index 8 (LEFT_BACK)
+	8,  // Physical button 8 -> Tyon index 8 (LEFT_BACK, switched)
 	13, // Physical button 9 -> Tyon index 13 (FIN_LEFT)
-	10, // Physical button 10 -> Tyon index 10 (RIGHT_BACK)
-	15, // Physical button 11 -> Tyon index 15 (WHEEL_DOWN)
-	12, // Physical button 12 -> Tyon index 12 (FIN_RIGHT)
+	12, // Physical button 10 -> Tyon index 12 (FIN_RIGHT)
+	4,  // Physical button 11 -> Tyon index 4 (THUMB_FORWARD)
+	3,  // Physical button 12 -> Tyon index 3 (THUMB_BACK, switched)
 	5,  // Physical button 13 -> Tyon index 5 (THUMB_PEDAL)
 	6,  // Physical button 14 -> Tyon index 6 (THUMB_PADDLE_UP)
-	4,  // Physical button 15 -> Tyon index 4 (THUMB_FORWARD)
+	7,  // Physical button 15 -> Tyon index 7 (THUMB_PADDLE_DOWN)
 };
 
 #endif
